78b8fe712ab487f7136e3ee055ce24146994f07c
Domi Cancino <cs.dominique17@gmail.com>
2023-11-07 10:27:17 -0300

    Se agregaron botones para sumar y restar

---
+++ src/components/Counter.vue
@@ -0,0 +1,61 @@
+<template>
+  
+    <h2>Counter</h2>
+    <p> {{  counter }}<sup>2</sup> = {{ SquareCounter }}</p>
+
+    <div>
+        <button>+1</button>
+        <button>-1</button>
+    </div>
+
+ 
+  
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+
+            counter: 5
+        }
+    },
+    methods:{
+        getSquareValue() {
+            return this.counter * this.counter
+        }
+
+        // increase() {
+        //     return this.counter + 1
+        // }
+    },
+    computed: {
+        SquareCounter() {
+            return this.counter * this.counter
+        }
+    }      
+}
+</script>
+
+
+<style>
+
+button{
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;  
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3 ease-in-out;
+}
+
+
+</style>
